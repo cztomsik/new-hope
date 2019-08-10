c1b32985267a1566eb77c214132593687f9d9f06
Kamil Tomšík <info@tomsik.cz>
2019-08-10 14:07:33 +0200

    fix some silly mistakes

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,19 @@
 // the idea is to get something working on osx, raspi & in a browser
 // improve it a bit and then port it back to the original project
 // as a replacement for webrender
+//
+// # Build
+// osx & raspi:
+//   cargo run
+//
+// emscripten (TODO):
+//   brew install emscripten
+//   cargo rustc --target=wasm32-unknown-emscripten -- -Clink-arg='-s' -Clink-arg='USE_SDL=2'
+//   http-server
+//   open http://127.0.0.1:8080/test.html
 
 use std::ffi::CString;
+use std::time::Instant;
 
 use std::mem;
 use std::ptr;
@@ -36,20 +47,26 @@ fn main() {
     let mut event_pump = sdl.event_pump().expect("init event pump");
 
     let window = video
-        .window("Test", WIDTH, 600)
+        .window("300", WIDTH, 400)
         .opengl()
         .resizable()
         .build()
         .expect("init window");
 
-    let gl_attr = video.gl_attr();
-    gl_attr.set_context_profile(GLProfile::Core);
+    if !EMSCRIPTEN {
+        let gl_attr = video.gl_attr();
+        gl_attr.set_context_profile(GLProfile::Core);        
+    }
 
     let gl_context = window.gl_create_context().expect("create context");
+
     gl::load_with(|name| video.gl_get_proc_address(name) as *const _);
     window.gl_make_current(&gl_context).expect("set context");
 
     let mut renderer = GlRenderer::new();
+
+    let mut time = Instant::now();
+    let mut frames: u128 = 0;
 
     loop {
         renderer.render();
@@ -63,11 +80,22 @@ fn main() {
             }
         }
 
-        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+        let elapsed = time.elapsed().as_nanos() as f32 / 1_000_000_000 as f32;
+
+        if elapsed > 1. {
+            println!("avg fps {}", frames as f32 / elapsed);
+            frames = 0;
+            time = Instant::now();
+        }
+
+        frames += 1;
+
+        // limit to 100 fps
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 100));
     }
 }
 
-const WIDTH: u32 = 800;
+const WIDTH: u32 = 300;
 
 struct GlRenderer {
     // simple pen to make it a bit more readable
@@ -106,8 +134,17 @@ impl GlRenderer {
                 2,
                 gl::FLOAT,
                 gl::FALSE,
-                0 as GLint,//(mem::size_of::<f32>() * 3) as GLint,
-                ptr::null(),
+                (mem::size_of::<f32>() * 5) as GLint,
+                0 as *const GLvoid,
+            );
+            gl::EnableVertexAttribArray(1);
+            gl::VertexAttribPointer(
+                1,
+                3,
+                gl::FLOAT,
+                gl::FALSE,
+                (mem::size_of::<f32>() * 5) as GLint,
+                (mem::size_of::<f32>() * 2) as *const GLvoid,
             );
             gl::DrawArrays(gl::TRIANGLES, 0, self.data.len() as i32);
         }
@@ -132,36 +169,38 @@ impl GlRenderer {
         self.text(brand_text, NAVBAR_TEXT_COLOR);
         self.br();
         self.br();
+        self.br();
     }
 
     fn h1(&mut self, text: &str) {
-        self.br();
         self.text(text, TEXT_COLOR);
+        self.br();
         self.br();
     }
 
     // where it is just do a round rect and don't change x/y (expects field)
     fn focus(&mut self) {
-        self.shadow(self.x, self.y, INPUT_WIDTH, INPUT_HEIGHT, 0.0, 1.0, FOCUS_COLOR);
+        self.shadow(self.x, self.y + 20., INPUT_WIDTH, INPUT_HEIGHT, 0.0, 1.0, FOCUS_COLOR);
     }
 
     fn field(&mut self, label: &str) {
-        self.br();
         self.text(label, TEXT_COLOR);
         self.br();
         self.fill_rect(self.x, self.y, INPUT_WIDTH, INPUT_HEIGHT, INPUT_COLOR);
         self.border(self.x, self.y, INPUT_WIDTH, INPUT_HEIGHT, INPUT_BORDER_COLOR);
+        self.y += INPUT_HEIGHT;
+        self.br();
     }
 
     fn button(&mut self, text: &str) {
-        let w = text.len() as f32 * 10.;
+        let w = text.len() as f32 * 15.;
         let h = 32.;
 
-        self.fill_round(0.0, 0.0, w, h, 4., BUTTON_COLOR);
-        self.x += BUTTON_PADDING;
+        self.fill_round(self.x, self.y, w, h, 4., BUTTON_COLOR);
+        self.x += 2. * BUTTON_PADDING;
         self.y += BUTTON_PADDING;
         self.text(text, BUTTON_TEXT_COLOR);
-        self.y -= BUTTON_PADDING;
+        self.x += 50.;
     }
 
     fn link(&mut self, text: &str) {
@@ -169,17 +208,17 @@ impl GlRenderer {
     }
 
     fn br(&mut self) {
-        self.x = 20.;
-        self.y += 16.;
+        self.x = 30.;
+        self.y += 20.;
     }
 
     fn text(&mut self, text: &str, color: Color) {
         let w = text.len() as f32 * 10.;
 
-        self.x += w;
-
         // line for now
-        self.fill_rect(self.x, self.y, w, 1., color);
+        self.fill_rect(self.x, self.y + 13., w, 1., color);
+
+        self.x += w;
     }
 
     fn shadow(&mut self, x: f32, y: f32, w: f32, h: f32, _blur: f32, spread: f32, color: Color) {
@@ -191,7 +230,7 @@ impl GlRenderer {
         self.fill_rect(x, y, w, 1., color);
         self.fill_rect(x, y, 1., h, color);
         self.fill_rect(x, y + h - 1., w, 1., color);
-        self.fill_rect(x, y + w - 1., 1., h, color);
+        self.fill_rect(x + w - 1., y, 1., h, color);
     }
 
     fn fill_round(&mut self, x: f32, y: f32, w: f32, h: f32, _radius: f32, color: Color) {
@@ -216,14 +255,14 @@ impl GlRenderer {
     }
 
     fn fill_triangle(&mut self, data: [(f32, f32, f32, f32, f32); 3]) {
-        println!("fill triangle {:?}", &data);
+        //println!("fill triangle {:?}", &data);
 
         for (x, y, r, g, b) in data.iter() {
             self.data.push(*x);
             self.data.push(*y);
-            //self.data.push(*r);
-            //self.data.push(*g);
-            //self.data.push(*b);
+            self.data.push(*r);
+            self.data.push(*g);
+            self.data.push(*b);
         }
     }
 }
@@ -231,31 +270,39 @@ impl GlRenderer {
 const VERTEX_SHADER_SOURCE: &str = r#"
   #version 100
 
-  attribute vec2 pos;
+  attribute vec2 a_pos;
+  attribute vec3 a_color;
+
+  varying vec3 v_color;
 
   void main() {
     // TODO: uniforms
-    vec2 size = vec2(800., 600.);
-    vec2 xy = (pos - (size / 2.)) / size;
+    vec2 size = vec2(300., 400.);
+    vec2 xy = (a_pos / (size / 2.)) - 1.;
     xy.y *= -1.;
 
     gl_Position = vec4(xy, 0.0, 1.0);
+    v_color = a_color;
   }
 "#;
 
 const FRAGMENT_SHADER_SOURCE: &str = r#"
   #version 100
 
+  precision mediump float;
+
+  varying vec3 v_color;
+
   void main() {
-    gl_FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+    gl_FragColor = vec4(v_color, 1.);
   }
 "#;
 
-const INPUT_WIDTH: f32 = 100.;
+const INPUT_WIDTH: f32 = 180.;
 const INPUT_HEIGHT: f32 = 28.;
 const INPUT_COLOR: Color = (1., 1., 1.);
 const _INPUT_TEXT_COLOR: Color = (0.3, 0.3, 0.3);
-const INPUT_BORDER_COLOR: Color = (0.7, 0.7, 0.7);
+const INPUT_BORDER_COLOR: Color = (0.8, 0.8, 0.8);
 const FOCUS_COLOR: Color = (0.7, 0.7, 1.0);
 
 const TEXT_COLOR: Color = (0., 0., 0.);
@@ -390,3 +437,8 @@ unsafe fn shader(shader_type: u32, source: &str) -> u32 {
 
     shader
 }
+
+#[cfg(target_os = "emscripten")]
+static EMSCRIPTEN: bool = true;
+#[cfg(not(target_os = "emscripten"))]
+static EMSCRIPTEN: bool = false;
